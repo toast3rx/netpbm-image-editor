e0238a2d033bbc1bb4e5c6ea9d6c40e658cacd7a
Mihai Latea <latea.alexandru@yahoo.com>
2021-12-27 15:25:08 +0200

    Add implementation for reading Load command

--- image_editor.c
+++ image_editor.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_WORD 12
+#define MAX_WORD 100
+#define MAX_FILENAME 100
 #define LOAD "LOAD"
 #define SELECT "SELECT"
 #define SELECT_ALL "SELECT ALL" // minor bug with length of string (MAX_WORD == 10)
@@ -12,13 +13,6 @@
 #define SAVE "SAVE"
 #define EXIT "EXIT"
 
-void read_command(char *command);
-void load_file();
-
-/**
- * @brief Used to determine image type
- * 
- */
 typedef enum
 {
     PPM,
@@ -41,7 +35,6 @@ typedef struct
     unsigned int value;
 } pgm_image;
 
-
 typedef struct
 {
     int rows;
@@ -55,10 +48,9 @@ typedef struct
 
 typedef struct
 {
-    FILE* file_in;
+    FILE *file_in;
     image_type type;
 
-    char *file_name;
     char magic_number[3];
     int width;
     int height;
@@ -69,21 +61,139 @@ typedef struct
         pgm_image pgm_img;
     } img;
     selection curr_selection;
-} image_file;
+} netpbm_image;
+
+/**
+ * @brief Split a string in words delimited by space
+ * 
+ * @param str string to be split
+ * @param words_array the array where the words will be stored
+ */
+void split_string(char *str, char **words_array)
+{
+    int j = 0;
+    int count = 0;
+    for (int i = 0; i <= (int)(strlen(str)); i++)
+    {
+        // if space or NULL found, assign NULL into splitStrings[cnt]
+        if (str[i] == ' ' || str[i] == '\0')
+        {
+            words_array[count][j] = '\0';
+            count++; //for next word
+            j = 0;   //for next word, init index to 0
+        }
+        else
+        {
+            words_array[count][j] = str[i];
+            j++;
+        }
+    }
+}
+
+/**
+ * @brief Read a command from stdin
+ * 
+ * @param command command to be read
+ * @return arguments of the command
+ */
+char **read_command(char *command, int *args)
+{
+    fgets(command, MAX_WORD, stdin);
+    command[strlen(command) - 1] = '\0';
+
+    // Count number of words and longest word for memory allocation
+    int count_words = 0;
+    int max_length = 0;
+    int length = 0;
+
+    for (int i = 0; i < (int)strlen(command); i++)
+    {
+        if (command[i] != ' ')
+            length++;
+        else
+        {
+            count_words++;
+            if (max_length < length)
+                max_length = length;
+            length = 0;
+        }
+    }
+    count_words++;
+    if (length > max_length)
+        max_length = length;
+
+    char **words = (char **)malloc(count_words * sizeof(char *));
+    if(!words) {
+        printf("Error!\n");
+        return NULL;
+    }
+    for (int i = 0; i < count_words; i++) {
+        words[i] = (char *)malloc((max_length + 1) * sizeof(char));
+        if(!words[i]) {
+            printf("Error\n");
+            return NULL;
+        }
+    }
+
+    split_string(command, words);
+
+    *args = count_words;
+    return words;
+}
+
+void load_file(char *file_name, netpbm_image *image)
+{
+    // Deschidem fisier
+    // Verific deshidere fisier
+    // Citim cifra magica
+    // Salvam latimea
+    // Salvam lungimea
+    // Salvam valoarea maxima pentru pixeli
+    // Salvam imaginea ca ppm sau pgm
+    // Salvam selectia initial fiind toata imaginea
+}
+
+/**
+ * @brief 
+ * Check if inserted command is the LOAD command
+ * @param command input from stdin
+ * @return 1 for true, 0 for false
+ */
+int is_load_command(char *command)
+{
+    if (strcmp(command, LOAD) == 0)
+        return 1;
+    else
+        return 0;
+}
+
+void free_2D_array(char **array, int length)
+{
+    for (int i = 0; i < length; i++)
+        free(array[i]);
+
+    free(array);
+}
 
 int main(void)
 {
 
     char *command = (char *)malloc((MAX_WORD + 1) * sizeof(char));
-
-    //FILE *in;
+    if(!command) {
+        printf("Error!\n");
+        return -1;
+    }
+    char **args;
+    int command_length = 0;
 
     do
     {
-        read_command(command);
-        if (strcmp(command, LOAD) == 0)
+        if (command_length > 0)
+            free_2D_array(args, command_length);
+        args = read_command(command, &command_length);
+        if (is_load_command(args[0]))
         {
-            load_file();
+            printf("LOADED\n");
         }
         else if (strcmp(command, SELECT) == 0)
         {
@@ -118,22 +228,6 @@ int main(void)
 
     //fclose(in);
     free(command);
+    free_2D_array(args, command_length);
     return 0;
-}
-
-/**
- * @brief Read a command from stdin
- * 
- * @param command command to be read
- */
-void read_command(char *command)
-{
-    fgets(command, MAX_WORD, stdin);
-    command[strlen(command) - 1] = '\0';
-}
-
-void load_file()
-{
-    // Deschidem fisier
-    // 
 }
